8b3a8a0304d2445ae0f34b585315f699f32a775a
Leonardo Giacone <leonardo@webiny.com>
2024-12-19 17:55:49 +0100

    feat(admin-ui): Tag + Icon Button components (#4423)

--- packages/admin-ui/src/index.ts
+++ packages/admin-ui/src/index.ts
@@ -18,6 +18,7 @@ export * from "./Separator";
 export * from "./Slider";
 export * from "./Switch";
 export * from "./Tabs";
+export * from "./Tag";
 export * from "./Text";
 export * from "./Textarea";
 export * from "./Toast";
