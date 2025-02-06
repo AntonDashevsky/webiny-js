c6dd8e06db2612ccf54cb708f004748c943be6f4
adrians5j <adrian@webiny.com>
2025-02-06 06:16:28 +0100

    wip

--- packages/admin-ui/src/Accordion/components/AccordionTrigger.tsx
+++ packages/admin-ui/src/Accordion/components/AccordionTrigger.tsx
@@ -5,43 +5,65 @@ import { cn, makeDecoratable } from "~/utils";
 import { type AccordionItemProps } from "./AccordionItem";
 import { AccordionItemAction } from "./AccordionItemAction";
 import { Icon } from "~/Icon";
+import { useMemo } from "react";
 
 type AccordionTriggerProps = Pick<
     AccordionItemProps,
     "title" | "description" | "icon" | "handle" | "actions"
 >;
 
-const AccordionTriggerBase = React.forwardRef<
-    React.ElementRef<typeof AccordionPrimitive.Trigger>,
-    AccordionTriggerProps
->(({ title, description, actions, icon, handle }, ref) => {
+const AccordionTriggerBase = ({
+    title,
+    description,
+    actions,
+    icon,
+    handle
+}: AccordionTriggerProps) => {
+    // The following three attributes are required for the trigger to act as a button.
+    // We can't use the default button element here because the content of the trigger
+    // can also contain one or more buttons.
+    const divAsButtonProps = useMemo<React.HTMLAttributes<HTMLDivElement>>(() => {
+        return {
+            role: "button",
+            tabIndex: 0,
+            onKeyDown: e => {
+                if (e.key === "Enter" || e.key === " ") {
+                    e.preventDefault();
+                    e.currentTarget.click();
+                }
+            }
+        };
+    }, []);
+
     const trigger = (
-        <AccordionPrimitive.Trigger
-            ref={ref}
-            className={cn(
-                "wby-w-full wby-flex wby-justify-between wby-items-center wby-px-md wby-py-sm-extra wby-cursor-pointer",
-                "focus-visible:wby-outline-none focus-visible:wby-border-none focus-visible:wby-ring-sm focus-visible:wby-ring-primary-dimmed",
-                "hover:wby-bg-neutral-dimmed",
-                "[&[data-state=open]_[data-role=open-close-indicator]]:wby-rotate-180"
-            )}
-        >
-            {icon && <div className={"wby-mr-md"}>{icon}</div>}
-            <div className={"wby-flex wby-flex-col wby-gap-xxs wby-flex-grow wby-text-left"}>
-                <div className={"wby-text-md wby-font-semibold"}>{title}</div>
-                <div className={"wby-text-sm wby-text-neutral-strong"}>{description}</div>
-            </div>
-            <div className={"wby-flex wby-ml-3xl wby-gap-xs"}>
-                {actions}
+        <AccordionPrimitive.Trigger asChild>
+            <div
+                {...divAsButtonProps}
+                className={cn(
+                    "wby-w-full wby-flex wby-justify-between wby-items-center wby-px-md wby-py-sm-extra wby-cursor-pointer",
+                    "focus-visible:wby-outline-none focus-visible:wby-border-none focus-visible:wby-ring-sm focus-visible:wby-ring-primary-dimmed",
+                    // "hover:wby-bg-neutral-dimmed",
+                    "[&[data-state=open]_[data-role=open-close-indicator]]:wby-rotate-180"
+                )}
+            >
+                {icon && <div className={"wby-mr-md"}>{icon}</div>}
+                <div className={"wby-flex wby-flex-col wby-gap-xxs wby-flex-grow wby-text-left"}>
+                    <div className={"wby-text-md wby-font-semibold"}>{title}</div>
+                    <div className={"wby-text-sm wby-text-neutral-strong"}>{description}</div>
+                </div>
+                <div className={"wby-flex wby-ml-3xl wby-gap-xs"}>
+                    {actions}
 
-                {/* No need to show the separator if there are no actions. */}
-                {actions && <AccordionItemAction.Separator />}
+                    {/* No need to show the separator if there are no actions. */}
+                    {actions && <AccordionItemAction.Separator />}
 
-                <Icon
-                    className={"wby-transition"}
-                    color={"neutral-strong"}
-                    data-role={"open-close-indicator"}
-                    icon={<KeyboardArrowDownIcon />}
-                />
+                    <Icon
+                        className={"wby-transition"}
+                        color={"neutral-strong"}
+                        data-role={"open-close-indicator"}
+                        icon={<KeyboardArrowDownIcon />}
+                    />
+                </div>
             </div>
         </AccordionPrimitive.Trigger>
     );
@@ -56,7 +78,7 @@ const AccordionTriggerBase = React.forwardRef<
     }
 
     return trigger;
-});
+};
 AccordionTriggerBase.displayName = AccordionPrimitive.Trigger.displayName;
 
 export const AccordionTrigger = makeDecoratable("AccordionTrigger", AccordionTriggerBase);
