1c05b9b9d91080c1811971bce2fb62b31ffaff63
adrians5j <adrian@webiny.com>
2025-02-10 07:52:04 +0100

    wip

--- packages/admin-ui/src/Accordion/components/AccordionTrigger.tsx
+++ packages/admin-ui/src/Accordion/components/AccordionTrigger.tsx
@@ -29,51 +29,50 @@ const AccordionTrigger = ({ title, description, actions, icon, handle }: Accordi
         };
     }, []);
 
-    const trigger = (
+    return (
         <AccordionPrimitive.Trigger asChild>
             <div
                 {...divAsButtonProps}
                 className={cn(
-                    "wby-w-full wby-flex wby-justify-between wby-items-center wby-px-md wby-py-sm-extra wby-cursor-pointer",
+                    "wby-w-full wby-flex wby-items-center wby-cursor-pointer",
                     "focus-visible:wby-outline-none focus-visible:wby-border-none focus-visible:wby-ring-sm focus-visible:wby-ring-primary-dimmed",
-                    // "hover:wby-bg-neutral-dimmed",
+                    "hover:wby-bg-neutral-dimmed",
                     "group-[.wby-accordion-variant-container]:wby-rounded-lg",
                     "[&[data-state=open]_[data-role=open-close-indicator]]:wby-rotate-180"
                 )}
             >
-                {icon && <div className={"wby-mr-md"}>{icon}</div>}
-                <div className={"wby-flex wby-flex-col wby-gap-xxs wby-flex-grow wby-text-left"}>
-                    <div className={"wby-text-md wby-font-semibold"}>{title}</div>
-                    <div className={"wby-text-sm wby-text-neutral-strong"}>{description}</div>
-                </div>
-                <div className={"wby-flex wby-ml-3xl wby-gap-xs"}>
-                    {actions}
+                {handle}
+                <div
+                    className={
+                        "wby-w-full wby-flex wby-justify-between wby-items-center wby-px-md wby-py-sm-extra"
+                    }
+                >
+                    {icon && <div className={"wby-mr-md"}>{icon}</div>}
+                    <div
+                        className={"wby-flex wby-flex-col wby-gap-xxs wby-flex-grow wby-text-left"}
+                    >
+                        <div className={"wby-text-md wby-font-semibold"}>{title}</div>
+                        <div className={"wby-text-sm wby-text-neutral-strong"}>{description}</div>
+                    </div>
+                    <div className={"wby-flex wby-ml-3xl wby-gap-xs"}>
+                        {actions}
 
-                    {/* No need to show the separator if there are no actions. */}
-                    {actions && <AccordionItemAction.Separator />}
+                        {/* No need to show the separator if there are no actions. */}
+                        {actions && <AccordionItemAction.Separator />}
 
-                    <Icon
-                        className={"wby-transition"}
-                        color={"neutral-strong"}
-                        data-role={"open-close-indicator"}
-                        label={"Open/close indicator"}
-                        icon={<KeyboardArrowDownIcon />}
-                    />
+                        <Icon
+                            size={"lg"}
+                            className={"wby-transition"}
+                            color={"neutral-strong"}
+                            data-role={"open-close-indicator"}
+                            label={"Open/close indicator"}
+                            icon={<KeyboardArrowDownIcon />}
+                        />
+                    </div>
                 </div>
             </div>
         </AccordionPrimitive.Trigger>
     );
-
-    if (handle) {
-        return (
-            <div className={"wby-flex wby-items-center"}>
-                <div>{handle}</div>
-                {trigger}
-            </div>
-        );
-    }
-
-    return trigger;
 };
 
 export { AccordionTrigger };
