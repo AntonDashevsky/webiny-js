99a290081bf60e43212175dca8b77487dc411da9
adrians5j <adrian@webiny.com>
2025-02-28 17:18:45 +0100

    wip

--- packages/admin-ui/src/Sidebar/components/SidebarMenuButton.tsx
+++ packages/admin-ui/src/Sidebar/components/SidebarMenuButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { cn, cva, type VariantProps } from "~/utils";
 
 const variants = cva(
@@ -11,23 +11,25 @@ const variants = cva(
         "focus-visible:wby-bg-neutral-dimmed",
         "data-[variant=group-label]:wby-opacity-30",
         "data-[active=true]:wby-bg-neutral-dimmed data-[active=true]:wby-font-semibold data-[active=true]:wby-pointer-events-none",
-        "disabled:wby-pointer-events-none disabled:wby-text-neutral-disabled",
-        "aria-disabled:wby-pointer-events-none disabled:wby-text-neutral-disabled",
         "[&>span:last-child]:wby-truncate [&>svg]:wby-shrink-0"
     ],
     {
         variants: {
             variant: {
                 "group-label": "wby-uppercase"
+            },
+            disabled: {
+                true: "wby-pointer-events-none wby-text-neutral-disabled"
             }
         }
     }
 );
 
 interface SidebarMenuButtonProps
-    extends React.ComponentProps<"button">,
+    extends React.ComponentProps<"div">,
         VariantProps<typeof variants> {
     active?: boolean;
+    disabled?: boolean;
     icon?: React.ReactNode;
     action?: React.ReactNode;
 }
@@ -35,22 +37,44 @@ interface SidebarMenuButtonProps
 const SidebarMenuButton = ({
     variant,
     active,
+    disabled,
     icon,
     action,
     className,
     children,
     ...props
 }: SidebarMenuButtonProps) => {
+    // The following three attributes are required for the trigger to act as a button.
+    // We can't use the default button element here because the content of the trigger
+    // can also contain one or more buttons.
+    const divAsButtonProps = useMemo<React.HTMLAttributes<HTMLDivElement>>(() => {
+        let tabIndex = 0;
+        if (variant === "group-label" || disabled) {
+            tabIndex = -1;
+        }
+
+        return {
+            role: "button",
+            tabIndex,
+            onKeyDown: e => {
+                if (e.key === "Enter" || e.key === " ") {
+                    e.preventDefault();
+                    e.currentTarget.click();
+                }
+            }
+        };
+    }, [variant, disabled]);
+
     return (
-        <button
+        <div
             data-sidebar="menu-button"
             data-active={active}
-            tabIndex={variant === "group-label" ? -1 : undefined}
-            className={cn(variants({ variant }), className)}
+            className={cn(variants({ variant, disabled }), className)}
             {...props}
+            {...divAsButtonProps}
         >
             {icon} {children} {action}
-        </button>
+        </div>
     );
 };
 
--- packages/admin-ui/src/Sidebar/components/SidebarMenuSubButton.tsx
+++ packages/admin-ui/src/Sidebar/components/SidebarMenuSubButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { cn, cva, type VariantProps } from "~/utils";
 
 const variants = cva(
@@ -10,8 +10,6 @@ const variants = cva(
         "hover:wby-bg-neutral-dimmed hover:!wby-no-underline",
         "focus-visible:wby-bg-neutral-dimmed",
         "data-[active=true]:wby-bg-neutral-dimmed data-[active=true]:wby-font-semibold data-[active=true]:wby-pointer-events-none",
-        "disabled:wby-pointer-events-none disabled:wby-text-neutral-disabled",
-        "aria-disabled:wby-pointer-events-none disabled:wby-text-neutral-disabled",
         "[&>span:last-child]:wby-truncate [&>svg]:wby-shrink-0",
         "group-data-[collapsible=icon]:wby-hidden"
     ],
@@ -20,15 +18,19 @@ const variants = cva(
             variant: {
                 "group-label":
                     "wby-uppercase wby-font-semibold wby-text-neutral-muted wby-text-sm wby-pt-md wby-pb-xs-plus wby-pointer-events-none"
+            },
+            disabled: {
+                true: "wby-pointer-events-none wby-text-neutral-disabled"
             }
         }
     }
 );
 
 interface SidebarMenuSubButtonProps
-    extends React.ComponentProps<"button">,
+    extends React.ComponentProps<"div">,
         VariantProps<typeof variants> {
     variant?: "group-label";
+    disabled?: boolean;
     active?: boolean;
     icon?: React.ReactNode;
 }
@@ -37,20 +39,42 @@ const SidebarMenuSubButton = ({
     variant,
     icon,
     active,
+    disabled,
     className,
     children,
     ...props
 }: SidebarMenuSubButtonProps) => {
+    // The following three attributes are required for the trigger to act as a button.
+    // We can't use the default button element here because the content of the trigger
+    // can also contain one or more buttons.
+    const divAsButtonProps = useMemo<React.HTMLAttributes<HTMLDivElement>>(() => {
+        let tabIndex = 0;
+        if (variant === "group-label" || disabled) {
+            tabIndex = -1;
+        }
+
+        return {
+            role: "button",
+            tabIndex: tabIndex,
+            onKeyDown: e => {
+                if (e.key === "Enter" || e.key === " ") {
+                    e.preventDefault();
+                    e.currentTarget.click();
+                }
+            }
+        };
+    }, [variant, disabled]);
+
     return (
-        <button
+        <div
             data-sidebar="menu-sub-button"
             data-active={active}
-            tabIndex={variant === "group-label" ? -1 : undefined}
-            className={cn(variants({ variant }), className)}
+            className={cn(variants({ variant, disabled }), className)}
             {...props}
+            {...divAsButtonProps}
         >
             {icon} {children}
-        </button>
+        </div>
     );
 };
 
