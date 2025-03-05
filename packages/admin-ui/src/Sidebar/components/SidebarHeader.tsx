c935d8d226a23b95a57077ae309a3e5f025b205e
adrians5j <adrian@webiny.com>
2025-03-05 14:19:51 +0100

    wip

--- packages/admin-ui/src/Sidebar/components/SidebarHeader.tsx
+++ packages/admin-ui/src/Sidebar/components/SidebarHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Separator } from "~/Separator";
 import { IconButton } from "~/Button";
 import { useSidebar } from "./SidebarProvider";
