201393a19a007b06448a1ba1459e5bba123e8be2
Leonardo Giacone <leonardo@webiny.com>
2025-04-04 11:40:39 +0200

    fix(admin-ui): various admin-ui fixes  (#4591)

--- packages/app-aco/src/components/Search/Search.tsx
+++ packages/app-aco/src/components/Search/Search.tsx
@@ -5,17 +5,19 @@ import { ReactComponent as SearchIcon } from "@webiny/icons/search.svg";
 interface SearchProps {
     value: string;
     onChange: (value: string) => void;
+    placeholder?: string;
 }
 
-export const Search = ({ value, onChange }: SearchProps) => {
+export const Search = ({ value, onChange, placeholder }: SearchProps) => {
     return (
         <Input
             value={value}
             onChange={onChange}
+            placeholder={placeholder}
             size={"md"}
-            variant={"secondary"}
+            variant={"ghost"}
             startIcon={<Icon label={"Search"} icon={<SearchIcon />} />}
-            className={"wby-w-[256px]"}
+            className={"wby-w-full"}
         />
     );
 };
