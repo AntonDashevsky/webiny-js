5c8ec5502010cf40451d7556dfd7478df470dab5
Pavel Denisjuk <pavel@webiny.com>
2025-06-08 22:53:50 +0200

    wip: add file gone missing during merge

---
+++ packages/api-aco/src/utils/decorators/FilterEntriesByFolderFactory.ts
@@ -0,0 +1,34 @@
+import type { FolderLevelPermissions } from "~/flp/index.js";
+import type { CmsEntry } from "@webiny/api-headless-cms/types/index.js";
+import { ROOT_FOLDER } from "~/constants.js";
+
+export class FilterEntriesByFolderFactory {
+    private readonly folderLevelPermissions: FolderLevelPermissions;
+
+    constructor(folderLevelPermissions: FolderLevelPermissions) {
+        this.folderLevelPermissions = folderLevelPermissions;
+    }
+
+    public async execute(entries: CmsEntry[]): Promise<CmsEntry[]> {
+        const results = await Promise.all(
+            entries.map(async entry => {
+                const folderId = entry.location?.folderId;
+                if (!folderId || folderId === ROOT_FOLDER) {
+                    return entry;
+                }
+
+                const permissions = await this.folderLevelPermissions.getFolderLevelPermissions(
+                    folderId
+                );
+                const canAccessFolderContent =
+                    await this.folderLevelPermissions.canAccessFolderContent({
+                        permissions,
+                        rwd: "r"
+                    });
+                return canAccessFolderContent ? entry : null;
+            })
+        );
+
+        return results.filter((entry): entry is CmsEntry => !!entry);
+    }
+}
