d5a87dfd9a0a2794d0401d2178ed266917c61061
adrians5j <adrian@webiny.com>
2025-06-25 12:06:14 +0200

    wip

--- packages/project/src/index.ts
+++ packages/project/src/index.ts
@@ -2,18 +2,17 @@ import { Project } from "./Project";
 
 async function main() {
     const project = Project.init();
-    const pro = await project.getProject();
-
-    console.log("1.", pro);
-
-    const api = await project.getApp("api");
-    console.log("2.", api);
+    // const pro = await project.getProject();
+    //
+    // console.log("1.", pro);
+    //
+    // const api = await project.getApp("api");
+    // console.log("2.", api);
 
     const apiPackages = await project.buildApp({
         app: "api",
         env: "dev"
     });
-    console.log(apiPackages);
 }
 
 await main();
--- packages/project/src/services/BuildAppService/packagesBuilders/worker.ts
+++ packages/project/src/services/BuildAppService/packagesBuilders/worker.ts
@@ -1,36 +1,33 @@
 import { serializeError } from "serialize-error";
-import { requireConfigWithExecute } from "./utils/requireConfig.js";
+import { requireConfigWithExecute } from "./utils/requireConfig.js"
 
 const worker = async () => {
     // Since this is executed in a separate process, we need to initialize the project.
-    // await initializeProject();
     const workerData = JSON.parse(process.argv[2]);
     const { package: pkg, env, variant, region, debug } = workerData;
     const options = {
-        cwd: pkg.paths.packageFolder,
+        cwd: pkg.paths.root,
         env,
         variant,
         region,
         debug
     };
 
-    console.log('pkg.paths.packageJsonFile', pkg.paths.packageJsonFile)
-    // const config = await requireConfigWithExecute(pkg.paths.packageJsonFile, {
-    //     options,
-    //     // context: getCli()
-    // });
-    //
-    // const hasBuildCommand = config.commands && typeof config.commands.build === "function";
-    // if (!hasBuildCommand) {
-    //     throw new Error("Build command not found.");
-    // }
-    // await config.commands.build(options).catch(error => {
-    //     // Send error message to the parent process
-    //     if (process.send) {
-    //         process.send(serializeError(error));
-    //     }
-    //     process.exit(1); // Ensure the worker process exits with an error code
-    // });
+    const config = await requireConfigWithExecute(pkg.paths.webinyConfigFile, {
+        options
+    });
+
+    const hasBuildCommand = config.commands && typeof config.commands.build === "function";
+    if (!hasBuildCommand) {
+        throw new Error("Build command not found.");
+    }
+    await config.commands.build(options).catch(error => {
+        // Send error message to the parent process
+        if (process.send) {
+            process.send(serializeError(error));
+        }
+        process.exit(1); // Ensure the worker process exits with an error code
+    });
 };
 
 (async () => {
