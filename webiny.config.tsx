daf6ecdcc5cd00c4417f400330b606f254070e4a
adrians5j <adrian@webiny.com>
2025-07-13 17:50:56 +0200

    wip

--- webiny.config.tsx
+++ webiny.config.tsx
@@ -5,10 +5,19 @@ export default function Webiny() {
     return (
         <>
             <Cli.Command name={"my-custom-command"} src={"./extensions/myCustomCommand.ts"} />
-            <Api.BeforeDeploy name={'my-api-before-deploy'} src={'./extensions/myApiBeforeDeploy.ts'} />
-            <Api.BeforeBuild name={'my-api-before-build'} src={'./extensions/myApiBeforeBuild.ts'} />
-            <Api.AfterDeploy name={'my-api-after-deploy'} src={'./extensions/myApiAfterDeploy.ts'} />
-            <Api.AfterBuild name={'my-api-after-build'} src={'./extensions/myApiAfterBuild.ts'} />
+            <Api.BeforeDeploy
+                name={"my-api-before-deploy"}
+                src={"./extensions/myApiBeforeDeploy.ts"}
+            />
+            <Api.BeforeBuild
+                name={"my-api-before-build"}
+                src={"./extensions/myApiBeforeBuild.ts"}
+            />
+            <Api.AfterDeploy
+                name={"my-api-after-deploy"}
+                src={"./extensions/myApiAfterDeploy.ts"}
+            />
+            <Api.AfterBuild name={"my-api-after-build"} src={"./extensions/myApiAfterBuild.ts"} />
             {/*<Project.Telemetry enabled={false}/>*/}
         </>
     );
