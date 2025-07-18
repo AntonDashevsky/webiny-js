63a07c7876240e1fd65a62b3b5dca64370033274
Pavel Denisjuk <pavel@webiny.com>
2025-07-18 11:58:14 +0200

    fix: ignore TS error on a peer dependency

--- packages/website-builder-nextjs/src/index.ts
+++ packages/website-builder-nextjs/src/index.ts
@@ -3,6 +3,7 @@ export * from "@webiny/website-builder-react";
 import { setHeadersProvider } from "@webiny/website-builder-react";
 
 setHeadersProvider(async () => {
+    // @ts-expect-error This is a peer dependency.
     const { headers } = await import("next/headers");
     return await headers();
 });
