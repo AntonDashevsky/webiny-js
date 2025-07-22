915e2ea85b1c2efd35d17f45054e8429df56e6ed
Pavel Denisjuk <pavel@webiny.com>
2025-07-22 18:10:42 +0200

    fix: skip preview link on folders

--- packages/app-website-builder/src/DocumentList/components/Table/Cells/CellStatus.tsx
+++ packages/app-website-builder/src/DocumentList/components/Table/Cells/CellStatus.tsx
@@ -3,14 +3,13 @@ import { IconButton, Tag } from "@webiny/admin-ui";
 import { ReactComponent as NewTab } from "@webiny/icons/open_in_new.svg";
 import { PageListConfig } from "~/configs/index.js";
 import { toTitleCaseLabel } from "~/shared/toTitleCaseLabel";
-import { usePagePreviewLink } from "~/DocumentList/hooks/useDocumentPreviewLink";
+import { usePagePreviewLink } from "~/DocumentList/hooks/usePagePreviewLink";
 import { PageDto } from "~/domain/Page";
 
 const { useTableRow, isFolderRow } = PageListConfig.Browser.Table.Column;
 
 export const CellStatus = () => {
     const { row } = useTableRow<{ data: PageDto }>();
-    const previewLink = usePagePreviewLink(row.data);
 
     if (isFolderRow(row)) {
         return <>{"-"}</>;
@@ -31,28 +30,33 @@ export const CellStatus = () => {
         return toTitleCaseLabel(row.data.status);
     }, [row.data.status]);
 
-    const openInNewTab = useCallback(() => {
-        if (previewLink) {
-            window.open(previewLink, "_blank");
-        }
-    }, [previewLink]);
-
     return (
         <div className={"wby-flex wby-items-center"}>
             <Tag
                 variant={variant}
                 content={`${statusLabel}${row.data.version ? ` (v${row.data.version})` : ""}`}
             />
-
-            {previewLink ? (
-                <IconButton
-                    onClick={openInNewTab}
-                    icon={<NewTab />}
-                    title={"Preview in new tab"}
-                    size={"sm"}
-                    variant={"ghost"}
-                />
-            ) : null}
+            <PreviewLink page={row.data} />
         </div>
     );
+};
+
+const PreviewLink = ({ page }: { page: PageDto }) => {
+    const previewLink = usePagePreviewLink(page);
+
+    const openInNewTab = useCallback(() => {
+        if (previewLink) {
+            window.open(previewLink, "_blank");
+        }
+    }, [previewLink]);
+
+    return previewLink ? (
+        <IconButton
+            onClick={openInNewTab}
+            icon={<NewTab />}
+            title={"Preview in new tab"}
+            size={"sm"}
+            variant={"ghost"}
+        />
+    ) : null;
 };
