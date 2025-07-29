4ba16d85421ab8c6b474455fed3d1c0cebd774ba
Pavel Denisjuk <pavel@webiny.com>
2025-07-29 20:21:21 +0200

    fix: use display:contents on editor UI groups

--- apps/admin/src/plugins/index.ts
+++ apps/admin/src/plugins/index.ts
@@ -1,20 +1,10 @@
 import { plugins } from "@webiny/plugins";
-import pageBuilderPlugins from "./pageBuilder";
-import formBuilderPlugins from "./formBuilder";
 import headlessCmsPlugins from "./headlessCms";
 
 // Imports plugins created via scaffolding utilities.
 import scaffoldsPlugins from "./scaffolds";
 
 plugins.register([
-    /**
-     * Page Builder app.
-     */
-    pageBuilderPlugins,
-    /**
-     * Form Builder app.
-     */
-    formBuilderPlugins,
     /**
      * Headless CMS app.
      */
