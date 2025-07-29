26dd9aa60459da116e6d931356635ef1884f1298
Pavel Denisjuk <pavel@webiny.com>
2025-07-29 12:42:41 +0200

    chore: remove obsolete apps from project template

--- packages/cwp-template-aws/template/ddb-os/apps/api/graphql/src/index.ts
+++ packages/cwp-template-aws/template/ddb-os/apps/api/graphql/src/index.ts
@@ -4,15 +4,6 @@ import graphqlPlugins from "@webiny/handler-graphql";
 import { createWcpContext, createWcpGraphQL } from "@webiny/api-wcp";
 import i18nPlugins from "@webiny/api-i18n/graphql";
 import i18nDynamoDbStorageOperations from "@webiny/api-i18n-ddb";
-import {
-    createPageBuilderContext,
-    createPageBuilderGraphQL
-} from "@webiny/api-page-builder/graphql";
-import { createStorageOperations as createPageBuilderStorageOperations } from "@webiny/api-page-builder-so-ddb-es";
-import pageBuilderPrerenderingPlugins from "@webiny/api-page-builder/prerendering";
-import pageBuilderImportExportPlugins from "@webiny/api-page-builder-import-export/graphql";
-import { createStorageOperations as createPageBuilderImportExportStorageOperations } from "@webiny/api-page-builder-import-export-so-ddb";
-import prerenderingServicePlugins from "@webiny/api-prerendering-service-aws/client";
 import dbPlugins from "@webiny/handler-db";
 import { DynamoDbDriver } from "@webiny/db-dynamodb";
 import dynamoDbPlugins from "@webiny/db-dynamodb/plugins";
@@ -21,20 +12,16 @@ import { createFileManagerContext, createFileManagerGraphQL } from "@webiny/api-
 import { createFileManagerStorageOperations } from "@webiny/api-file-manager-ddb";
 import logsPlugins from "@webiny/handler-logs";
 import fileManagerS3, { createAssetDelivery } from "@webiny/api-file-manager-s3";
-import { createFormBuilder } from "@webiny/api-form-builder";
-import { createFormBuilderStorageOperations } from "@webiny/api-form-builder-so-ddb-es";
 import { createHeadlessCmsContext, createHeadlessCmsGraphQL } from "@webiny/api-headless-cms";
 import { createStorageOperations as createHeadlessCmsStorageOperations } from "@webiny/api-headless-cms-ddb-es";
 import { createHcmsTasks } from "@webiny/api-headless-cms-tasks-ddb-es";
 import { createAco } from "@webiny/api-aco";
-import { createAcoPageBuilderContext } from "@webiny/api-page-builder-aco";
 import { createAcoHcmsContext } from "@webiny/api-headless-cms-aco";
 import securityPlugins from "./security";
 import tenantManager from "@webiny/api-tenant-manager";
 import { createAuditLogs } from "@webiny/api-audit-logs";
 import { createBackgroundTasks } from "@webiny/api-background-tasks-os";
-import { createApwGraphQL, createApwPageBuilderContext } from "@webiny/api-apw";
-import { createStorageOperations as createApwSaStorageOperations } from "@webiny/api-apw-scheduler-so-ddb";
+import { createApwGraphQL } from "@webiny/api-apw";
 import { createWebsockets } from "@webiny/api-websockets";
 import { createRecordLocking } from "@webiny/api-record-locking";
 import { createLogger } from "@webiny/api-log";
@@ -89,35 +76,10 @@ export const handler = createHandler({
         createFileManagerGraphQL(),
         createAssetDelivery({ documentClient }),
         fileManagerS3(),
-        prerenderingServicePlugins({
-            eventBus: String(process.env.EVENT_BUS)
-        }),
-        createPageBuilderContext({
-            storageOperations: createPageBuilderStorageOperations({
-                documentClient,
-                elasticsearch: elasticsearchClient,
-                plugins: []
-            })
-        }),
-        createPageBuilderGraphQL(),
-        pageBuilderPrerenderingPlugins(),
-        pageBuilderImportExportPlugins({
-            storageOperations: createPageBuilderImportExportStorageOperations({ documentClient })
-        }),
-        createFormBuilder({
-            storageOperations: createFormBuilderStorageOperations({
-                documentClient,
-                elasticsearch: elasticsearchClient
-            })
-        }),
         createApwGraphQL(),
-        createApwPageBuilderContext({
-            storageOperations: createApwSaStorageOperations({ documentClient })
-        }),
         createAco({
             documentClient
         }),
-        createAcoPageBuilderContext(),
         createAcoHcmsContext(),
         createHcmsTasks(),
         createAuditLogs(),
--- packages/serverless-cms-aws/src/api/plugins/handlersPaths.ts
+++ packages/serverless-cms-aws/src/api/plugins/handlersPaths.ts
@@ -1,8 +1,4 @@
 export const HANDLERS_PATHS = [
-    ["pageBuilder", "export", "combine"],
-    ["pageBuilder", "export", "process"],
-    ["pageBuilder", "import", "create"],
-    ["pageBuilder", "import", "process"],
     ["apw", "executeAction"],
     ["apw", "scheduleAction"],
     ["migration"]
