e0bfc55d5a4d6a42b32e6558d9fb2eb6753e331b
Pavel Denisjuk <pavel@webiny.com>
2025-08-08 15:30:29 +0200

    fix(website-builder-sdk): disable pino in production

--- packages/website-builder-sdk/src/Logger.ts
+++ packages/website-builder-sdk/src/Logger.ts
@@ -1,9 +1,9 @@
 import pino from "pino";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const logger = pino({
     name: "Website Builder SDK",
-    level: "debug",
-    transport: {
-        target: "pino-pretty"
-    }
+    level: isProduction ? "silent" : "debug",
+    ...(isProduction ? {} : { transport: { target: "pino-pretty" } })
 });
