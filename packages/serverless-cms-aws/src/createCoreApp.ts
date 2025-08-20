6939419eaed93b40e3c5b719923563cb4bd719f1
adrians5j <adrian@webiny.com>
2025-08-20 10:44:03 +0200

    wip

--- packages/serverless-cms-aws/src/createCoreApp.ts
+++ packages/serverless-cms-aws/src/createCoreApp.ts
@@ -26,26 +26,26 @@ export function createCoreApp(projectAppParams: CreateCoreAppParams = {}) {
             const { createCorePulumiApp } = await import("@webiny/pulumi-aws");
 
             return createCorePulumiApp(projectAppParams);
-        },
-        async getPlugins() {
-            // eslint-disable-next-line import/dynamic-import-chunkname
-            const { generateDdbToEsHandler, checkEsServiceRole, checkOsServiceRole } = await import(
-                "./core/plugins/index.js"
-            );
-
-            const builtInPlugins = [];
-            if (projectAppParams.elasticSearch || projectAppParams.openSearch) {
-                builtInPlugins.push(generateDdbToEsHandler);
-                if (projectAppParams.elasticSearch) {
-                    builtInPlugins.push(checkEsServiceRole);
-                } else {
-                    builtInPlugins.push(checkOsServiceRole);
-                }
-            }
-
-            const customPlugins = projectAppParams.plugins ? [...projectAppParams.plugins] : [];
-
-            return [builtInPlugins, customPlugins];
         }
+        // async getPlugins() {
+        //     // eslint-disable-next-line import/dynamic-import-chunkname
+        //     const { generateDdbToEsHandler, checkEsServiceRole, checkOsServiceRole } = await import(
+        //         "./core/plugins/index.js"
+        //     );
+        //
+        //     const builtInPlugins = [];
+        //     if (projectAppParams.elasticSearch || projectAppParams.openSearch) {
+        //         builtInPlugins.push(generateDdbToEsHandler);
+        //         if (projectAppParams.elasticSearch) {
+        //             builtInPlugins.push(checkEsServiceRole);
+        //         } else {
+        //             builtInPlugins.push(checkOsServiceRole);
+        //         }
+        //     }
+        //
+        //     const customPlugins = projectAppParams.plugins ? [...projectAppParams.plugins] : [];
+        //
+        //     return [builtInPlugins, customPlugins];
+        // }
     };
 }
