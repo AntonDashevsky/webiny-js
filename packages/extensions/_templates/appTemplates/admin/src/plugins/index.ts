7ec9796fb0ec89c10e58f7b48d9813d284456ba3
adrians5j <adrian@webiny.com>
2025-08-22 09:49:35 +0200

    wip

--- packages/extensions/_templates/appTemplates/admin/src/plugins/index.ts
+++ packages/extensions/_templates/appTemplates/admin/src/plugins/index.ts
@@ -1,26 +1,9 @@
 import { plugins } from "@webiny/plugins";
 import pageBuilderPlugins from "./pageBuilder";
-import headlessCmsPlugins from "./headlessCms";
-import theme from "theme";
-
-// Imports plugins created via scaffolding utilities.
-import scaffoldsPlugins from "./scaffolds";
 
 plugins.register([
     /**
      * Page Builder app plugins.
      */
-    pageBuilderPlugins,
-    /**
-     * Headless CMS app plugins.
-     */
-    headlessCmsPlugins,
-    /**
-     * App theme controls page builder and form builder layouts, styles, etc.
-     */
-    theme(),
-    /**
-     * Plugins created via scaffolding utilities.
-     */
-    scaffoldsPlugins()
+    pageBuilderPlugins
 ]);
