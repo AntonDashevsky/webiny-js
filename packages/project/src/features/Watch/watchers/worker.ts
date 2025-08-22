63a276f69ddb4bfd7a3724028845da8fcca004b4
adrians5j <adrian@webiny.com>
2025-08-22 08:39:34 +0200

    wip

--- packages/project/src/features/Watch/watchers/worker.ts
+++ packages/project/src/features/Watch/watchers/worker.ts
@@ -1,7 +1,6 @@
-import "tsx/esm";
+import "tsx";
 import { serializeError } from "serialize-error";
 import { requireConfigWithExecute } from "~/utils/index.js";
-import chalk from "chalk";
 
 const workerData = JSON.parse(process.argv[2]);
 const { package: pkg, env, variant, region, debug } = workerData;
@@ -12,18 +11,16 @@ const config = await requireConfigWithExecute(pkg.paths.webinyConfigFile, {
     options
 });
 
-const hasWatch = config.commands && typeof config.commands.watch === "function";
-if (hasWatch) {
-    // TODO: remove {} as any
-    config.commands.watch(options, {} as any).catch(error => {
-        // Send error message to the parent process
-        process.send!(serializeError(error));
-        process.exit(1); // Ensure the worker process exits with an error code
-    });
-} else {
-    process.stdout.write(
-        `Skipping watch; ${chalk.yellow(
-            "watch"
-        )} command is missing. Check package's ${chalk.yellow("webiny.config.ts")} file.`
-    );
+try {
+    const hasWatch = config.commands && typeof config.commands.watch === "function";
+    if (hasWatch) {
+        await config.commands.watch(options, {});
+    } else {
+        throw new Error("Watch command not found.");
+    }
+} catch (error) {
+    if (process.send) {
+        process.send({ type: "error", error: serializeError(error) });
+    }
+    process.exit(1); // Ensure the worker process exits with an error code
 }
