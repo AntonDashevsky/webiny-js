d24d47eb893b5f4ea8ae5872781cfd2b61a9d779
adrians5j <adrian@webiny.com>
2025-08-26 12:13:49 +0200

    wip

--- packages/project/src/features/Watch/watchers/worker.ts
+++ packages/project/src/features/Watch/watchers/worker.ts
@@ -1,5 +1,4 @@
 import "tsx";
-import { serializeError } from "serialize-error";
 import { requireConfigWithExecute } from "~/utils/index.js";
 
 const workerData = JSON.parse(process.argv[2]);
@@ -11,16 +10,9 @@ const config = await requireConfigWithExecute(pkg.paths.webinyConfigFile, {
     options
 });
 
-try {
-    const hasWatch = config.commands && typeof config.commands.watch === "function";
-    if (hasWatch) {
-        await config.commands.watch(options, {});
-    } else {
-        throw new Error("Watch command not found.");
-    }
-} catch (error) {
-    if (process.send) {
-        process.send({ type: "error", error: serializeError(error) });
-    }
-    process.exit(1); // Ensure the worker process exits with an error code
+const hasWatch = config.commands && typeof config.commands.watch === "function";
+if (hasWatch) {
+    await config.commands.watch(options, {});
+} else {
+    throw new Error("Watch command not found.");
 }
