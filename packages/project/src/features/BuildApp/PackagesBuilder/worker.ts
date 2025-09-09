328b3d512eba2913a4065a43b66fe48397042ed6
adrians5j <adrian@webiny.com>
2025-09-09 14:59:16 +0200

    wip

--- packages/project/src/features/BuildApp/PackagesBuilder/worker.ts
+++ packages/project/src/features/BuildApp/PackagesBuilder/worker.ts
@@ -1,24 +1,31 @@
 import "tsx";
 import { requireConfigWithExecute } from "./utils/requireConfig.js";
+import { serializeError } from "serialize-error";
 
-const workerData = JSON.parse(process.argv[2]);
+try {
+    const workerData = JSON.parse(process.argv[2]);
 
-const { package: pkg, env, variant, region, debug } = workerData;
-const options = {
-    cwd: pkg.paths.packageFolder,
-    env,
-    variant,
-    region,
-    debug
-};
+    const { package: pkg, env, variant, region, debug } = workerData;
+    const options = {
+        cwd: pkg.paths.packageFolder,
+        env,
+        variant,
+        region,
+        debug
+    };
 
-const config = await requireConfigWithExecute(pkg.paths.webinyConfigFile, {
-    options
-});
+    const config = await requireConfigWithExecute(pkg.paths.webinyConfigFile, {
+        options
+    });
 
-const hasBuild = config.commands && typeof config.commands.build === "function";
-if (!hasBuild) {
-    throw new Error("Build command not found.");
+    const hasBuild = config.commands && typeof config.commands.build === "function";
+    if (!hasBuild) {
+        throw new Error("Build command not found.");
+    }
+
+    await config.commands.build(options);
+} catch (e) {
+    if (process.send) {
+        process.send({ error: serializeError(e) });
+    }
 }
-
-await config.commands.build(options);
