e8e48d1a10bd9a2ae679e6787477f60b50588e4a
adrians5j <adrian@webiny.com>
2025-09-12 13:26:08 +0200

    adio

--- extensions/myAdminExtension.tsx
+++ extensions/myAdminExtension.tsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { AddLogo } from "@webiny/app-serverless-cms";
 
 import logoPng from "./logo.png";
 
 export const Extension = () => {
-    return (
-        <>
-            <AddLogo logo={<img src={logoPng} height={40} width={40} />} />
-        </>
-    );
+    return <></>;
 };
