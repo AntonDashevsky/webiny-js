a7cb970f1df8badafc13f4870ef3939f922c5113
adrians5j <adrian@webiny.com>
2025-09-12 16:52:06 +0200

    fix: remove 'no pkgs' msg (just confuses user)

--- packages/cli-core/src/features/BuildCommand/buildRunners/ZeroBuildsRunner.ts
+++ packages/cli-core/src/features/BuildCommand/buildRunners/ZeroBuildsRunner.ts
@@ -2,6 +2,6 @@ import { BaseBuildRunner } from "./BaseBuildRunner.js";
 
 export class ZeroBuildsRunner extends BaseBuildRunner {
     public override async run() {
-        this.ui.info(`No packages to build.`);
+        // Do nothing.
     }
 }
