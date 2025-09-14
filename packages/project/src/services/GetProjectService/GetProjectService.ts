b719a6793936f07d40cdaadf2048c679d1e14344
adrians5j <adrian@webiny.com>
2025-09-14 16:30:46 +0200

    feat: integrate `GetProjectVersionService` into `GetProjectService` and update dependencies

--- packages/project/src/services/GetProjectService/GetProjectService.ts
+++ packages/project/src/services/GetProjectService/GetProjectService.ts
@@ -1,13 +1,15 @@
 import { createImplementation } from "@webiny/di-container";
 import findUp from "find-up";
 import { dirname } from "path";
-import { GetProjectService } from "~/abstractions/index.js";
+import { GetProjectService, GetProjectVersionService } from "~/abstractions/index.js";
 import { ProjectModel } from "~/models/ProjectModel.js";
 import { PathModel } from "~/models/PathModel.js";
 import path from "path";
 
 export class DefaultGetProjectService implements GetProjectService.Interface {
     cachedProject: ProjectModel | null = null;
+
+    constructor(private getProjectVersionService: GetProjectVersionService.Interface) {}
 
     execute(cwd = process.cwd()) {
         if (this.cachedProject) {
@@ -27,9 +29,12 @@ export class DefaultGetProjectService implements GetProjectService.Interface {
         const workspacesFolderPath = projectRootFolderPath.join(".webiny", "workspaces");
         const localPulumiStateFilesFolderPath = projectRootFolderPath.join(".pulumi");
 
+        const projectVersion = this.getProjectVersionService.execute(cwd);
+
         this.cachedProject = new ProjectModel({
             // TODO: read project name from config file.
             name: path.basename(projectRootFolderPath.toString()),
+            version: projectVersion,
             paths: {
                 dotWebinyFolder: dotWebinyFolderPath,
                 rootFolder: projectRootFolderPath,
@@ -46,5 +51,5 @@ export class DefaultGetProjectService implements GetProjectService.Interface {
 export const getProjectService = createImplementation({
     abstraction: GetProjectService,
     implementation: DefaultGetProjectService,
-    dependencies: []
+    dependencies: [GetProjectVersionService]
 });
