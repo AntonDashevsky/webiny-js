069654d805ba46f825c86624232253b75b8cd217
adrians5j <adrian@webiny.com>
2025-09-18 19:19:08 +0200

    chore: remove unused test file `test.ts` from `project` package

--- packages/project/src/test.ts
+++
@@ -1,8 +0,0 @@
-import { ProjectSdk } from "./ProjectSdk.js";
-
-const projectSdk = await ProjectSdk.init();
-
-process.env.AWS_REGION = "eu-central-1";
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const rez = await projectSdk.wcp.login();
