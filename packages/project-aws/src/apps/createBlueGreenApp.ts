3812f28a2c68967be6f557b90eba31345d480ac5
adrians5j <adrian@webiny.com>
2025-09-22 11:42:57 +0200

    refactor: remove unused code and legacy plugin setup in `createBlueGreenApp`

--- packages/project-aws/src/apps/createBlueGreenApp.ts
+++ packages/project-aws/src/apps/createBlueGreenApp.ts
@@ -1,95 +1,6 @@
-import type { CreateBlueGreenPulumiAppParams } from "~/pulumi/apps/blueGreen/createBlueGreenPulumiApp.js";
-import { createBlueGreenPulumiApp } from "~/pulumi/apps/blueGreen/createBlueGreenPulumiApp.js";
-import type { PluginCollection } from "@webiny/plugins/types.js";
-export interface CreateBlueGreenAppParams extends CreateBlueGreenPulumiAppParams {
-    plugins?: PluginCollection;
-}
+import { createBlueGreenPulumiApp } from "~/pulumi/apps/blueGreen/createBlueGreenPulumiApp";
 
-// export interface IEnvironment {
-//     name: string;
-//     env: string;
-//     variant: string | undefined;
-//     domains: {
-//         [key: string]: string;
-//     };
-// }
-
-// const builtInPlugins: Plugin[] = [
-//     createBeforeDeployPlugin(async ({ env, variant }, context) => {
-//         if (!variant?.length) {
-//             return;
-//         }
-//         const message = `Cannot deploy Blue / Green system environment (${env}) with a variant (${variant}).`;
-//         context.error(message);
-//         throw new Error(message);
-//     }),
-//     createAfterDeployPlugin(async ({ env }) => {
-//         const bg = await getStackOutput<IBlueGreenStackOutput>({
-//             app: "blueGreen",
-//             env,
-//             /**
-//              * Blue / Green system cannot have any variants.
-//              */
-//             variant: undefined
-//         });
-//
-//         if (!bg) {
-//             return;
-//         }
-//
-//         const domains = Array.isArray(bg.domains) ? bg.domains : [];
-//
-//         const environments = (bg.environments || []).reduce<IEnvironment[]>((items, item) => {
-//             const index = items.findIndex(i => i.name === item.name);
-//             if (index >= 0) {
-//                 items[index].domains[item.type] = item.target;
-//
-//                 return items;
-//             }
-//
-//             items.push({
-//                 name: item.name,
-//                 env: item.env,
-//                 variant: item.variant,
-//                 domains: {
-//                     [item.type]: item.target
-//                 }
-//             });
-//
-//             return items;
-//         }, []);
-//
-//         const output = [
-//             "",
-//             green(`Blue / Green Router`),
-//             `‣ Environment name: ${blue(env)}`,
-//             `‣ CloudFront domain: ${bg.distributionDomain}`,
-//             `‣ CloudFront URL: ${bg.distributionUrl}`,
-//             "",
-//             `‣ Domains attached: `,
-//             ...domains.map(domain => `  - https://${domain}`),
-//             "",
-//             `‣ Environments: `,
-//             ...environments.map(item => {
-//                 const envVariant = `env: ${item.env}${
-//                     item.variant ? ` / variant: ${item.variant}` : ""
-//                 }`;
-//                 return [
-//                     `  - ${blue(item.name)} (${envVariant})`,
-//                     ...Object.keys(item.domains).map(type => {
-//                         return `    - ${type}: https://${item.domains[type]}`;
-//                     })
-//                 ].join("\n");
-//             }),
-//             ""
-//         ];
-//         console.log(output.join("\n"));
-//     })
-// ];
-
-export function createBlueGreenApp(projectAppParams: CreateBlueGreenAppParams) {
-    // const plugins = projectAppParams.plugins ? [...projectAppParams.plugins] : [];
-
+export function createBlueGreenApp() {
     return {
         id: "blueGreen",
         name: "Blue / Green System",
@@ -103,7 +14,6 @@ export function createBlueGreenApp(projectAppParams: CreateBlueGreenAppParams) {
                 function: "none"
             }
         },
-        pulumi: createBlueGreenPulumiApp(projectAppParams)
-        // plugins: plugins.concat([builtInPlugins])
+        pulumi: createBlueGreenPulumiApp()
     };
 }
