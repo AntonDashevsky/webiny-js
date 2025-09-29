8008cdc21d86622d994890f602924c0ae8cdcf96
adrians5j <adrian@webiny.com>
2025-09-29 08:38:12 +0200

    feat: improve GracefulError so that it allows for formatted error messages
    This is mainly useful for CLI apps, where you want certain words to be colored.

--- packages/project/src/GracefulError.ts
+++ packages/project/src/GracefulError.ts
@@ -1,7 +1,27 @@
+import chalk from "chalk";
+import util from "util";
+
 export class GracefulError extends Error {
-    static from(error: Error): GracefulError {
-        return new GracefulError(error.message, {
-            cause: error
+    static from(cause: Error, ...message: string[]): GracefulError;
+    static from(...message: string[]): GracefulError;
+
+    static from(causeOrMessage: Error | string, ...message: string[]): GracefulError {
+        if (causeOrMessage instanceof Error) {
+            const formattedMessage = this.formatMessage(...message);
+            return new GracefulError(formattedMessage, { cause: causeOrMessage });
+        }
+
+        const formattedMessage = this.formatMessage(...[causeOrMessage, ...message]);
+        return new GracefulError(formattedMessage);
+    }
+
+    private static formatMessage(...message: string[]): string {
+        const [text, ...args] = message;
+        // Replace all placeholders (match with `/%[a-zA-Z]/g` regex) with colorized values.
+        const messageWithColorizedPlaceholders = text.replace(/%[a-zA-Z]/g, match => {
+            return chalk.cyan(match);
         });
+
+        return util.format(messageWithColorizedPlaceholders, ...args);
     }
 }
