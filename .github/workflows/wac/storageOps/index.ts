675d429c5027a2fe9e8e688c65d5515206869b26
adrians5j <adrian@webiny.com>
2025-09-29 11:05:55 +0200

    ci: export `AbstractStorageOps` from `index.ts`

--- .github/workflows/wac/storageOps/index.ts
+++ .github/workflows/wac/storageOps/index.ts
@@ -1,3 +1,4 @@
 export { DdbStorageOps } from "./DdbStorageOps.js";
 export { DdbEsStorageOps } from "./DdbEsStorageOps.js";
 export { DdbOsStorageOps } from "./DdbOsStorageOps.js";
+export { AbstractStorageOps } from "./AbstractStorageOps.js";
