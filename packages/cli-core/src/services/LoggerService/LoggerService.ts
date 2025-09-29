edfb57611886edfe7b74acc951fad87231d9e6cd
adrians5j <adrian@webiny.com>
2025-09-29 10:03:53 +0200

    refactor: improve logging setup by dynamically determining project root and simplifying log level handling

--- packages/cli-core/src/services/LoggerService/LoggerService.ts
+++ packages/cli-core/src/services/LoggerService/LoggerService.ts
@@ -1,27 +1,20 @@
 import { createImplementation } from "@webiny/di-container";
-import { createPinoLogger as baseCreatePinoLogger, Logger } from "@webiny/logger";
+import { createPinoLogger as baseCreatePinoLogger, type Logger } from "@webiny/logger";
 import { LoggerService } from "~/abstractions/index.js";
 import * as fs from "node:fs";
 import path from "node:path";
+import findUp from "find-up";
+
+const DEFAULT_LOG_LEVEL = "info";
 
 export class DefaultLoggerService implements LoggerService.Interface {
     pinoLogger: Logger;
 
     constructor() {
-        // Wanted to use GetProjectSdkService to get project root path, but
-        // to get that, had to call async method, which is not allowed in constructor.
-        // TODO: implement a better way to get project root path.
-        const logFilePath = path.join(process.cwd(), "logs.txt");
+        const logStream = this.getLogStream();
 
-        // Ensure the file exists or can be appended to
-        const logStream = fs.createWriteStream(logFilePath, { flags: "a" });
-
-        this.pinoLogger = baseCreatePinoLogger(
-            {
-                level: process.env.LOG_LEVEL || "info"
-            },
-            logStream // write directly to file instead of console
-        );
+        const level = process.env.LOG_LEVEL || DEFAULT_LOG_LEVEL;
+        this.pinoLogger = baseCreatePinoLogger({ level }, logStream);
     }
 
     trace(message?: any, ...optionalParams: any[]) {
@@ -50,6 +43,37 @@ export class DefaultLoggerService implements LoggerService.Interface {
 
     log(message?: any, ...optionalParams: any[]) {
         this.pinoLogger.info(message, ...optionalParams);
+    }
+
+    private getLogStream() {
+        // Wanted to use `GetProjectSdkService` to get project root path, but
+        // to get that, had to call async method, which is not allowed in constructor.
+        // TODO: implement a better way to get project root path.
+        const webinyConfigPath = findUp.sync("webiny.config.tsx");
+        if (!webinyConfigPath) {
+            // Should not happen, but just in case.
+            throw new Error("Could not find project root path.");
+        }
+
+        const projectRootPath = path.dirname(webinyConfigPath!);
+
+        const logsFolderPath = path.join(projectRootPath, ".webiny", "logs");
+        const logsFileName = this.getLogFileName();
+
+        if (!fs.existsSync(logsFolderPath)) {
+            fs.mkdirSync(logsFolderPath, { recursive: true });
+        }
+
+        const logFilePath = path.join(logsFolderPath, logsFileName);
+
+        // Ensure the file exists or can be appended to
+        return fs.createWriteStream(logFilePath, { flags: "a" });
+    }
+
+    private getLogFileName() {
+        const now = new Date();
+        const dateStr = now.toISOString().split("T")[0];
+        return `logs-${dateStr}.log`;
     }
 }
 
