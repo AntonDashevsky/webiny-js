040c9bec44263dd99a8e5937a6c52826efb3ce2e
adrians5j <adrian@webiny.com>
2025-10-02 19:14:34 +0200

    test: remove `ResizeObserver loop` workaround in Cypress config

--- cypress-tests/cypress/support/e2e.js
+++ cypress-tests/cypress/support/e2e.js
@@ -1,6 +1,3 @@
 import "@4tw/cypress-drag-drop";
 import "@testing-library/cypress/add-commands";
 import "./commands";
-
-// Workaround for https://github.com/cypress-io/cypress/issues/29277
-Cypress.on("uncaught:exception", err => !err.message.includes("ResizeObserver loop"));
